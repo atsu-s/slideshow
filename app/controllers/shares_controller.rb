da8360d4493d55b7f4da7d8106f7f4b3d6f058fa
曽根惇史 <atsu@soneatsushinoMacBook-Air.local>
2021-05-20 18:07:04 +0900

    公開機能実装

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,2 +1,29 @@
 class SharesController < ApplicationController
+  before_action :set_slide, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @share = Share.new
+  end
+
+  def create
+    @share = Share.new(share_params)
+    if @share.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def share_params
+    params.require(:share).permit(:text).merge(slide_id: @slide.id)
+  end
+
+  def set_slide
+    @slide = Slide.find_by(id: params[:slide_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :slides do
     resources :images
+    resources :shares
   end
 end
