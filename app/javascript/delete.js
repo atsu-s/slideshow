2bd36b08fde693c5aaa5a0aa587fa7501931bedc
曽根惇史 <atsu@soneatsushinoMacBook-Air.local>
2021-05-27 11:52:12 +0900

    最終調整

--- app/javascript/delete.js
+++ app/javascript/delete.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function(){
   const slideDestroy = document.getElementById("slide_destroy")
   const cancelButton = document.getElementById("cancel_btn")
   const deleteSubmit = document.getElementById("delete")
-  const rollBack = document.getElementById("roll_back")
+  const rollBackButton = document.getElementById("roll_back_btn")
 
   deleteButton.addEventListener('click', function(){
     showScreen.setAttribute("style", "display:none;");
@@ -14,7 +14,7 @@ window.addEventListener('load', function(){
     slideDestroy.setAttribute("style", "display:flex;");
     cancelButton.setAttribute("style", "display:inline;");
     deleteSubmit.setAttribute("style", "display:inline;");
-    rollBack.setAttribute("style", "display:none;");
+    rollBackButton.setAttribute("style", "display:none;");
   
     cancelButton.addEventListener('click', function(){
       window.location.reload();
--- app/javascript/rollback.js
+++ app/javascript/rollback.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const rollBack = document.getElementById("roll_back")
-  const rollBackButton = document.getElementById("roll_back_btn")
 
-  rollBack.addEventListener('mouseover', function(){
+  const rollBackButton = document.getElementById("roll_back_btn")
+  const backButton = document.getElementById("back_btn")
+
+
+  rollBackButton.addEventListener('mouseover', function(){
     this.setAttribute("style", "opacity: 0.7;")
 
   });
 
-  rollBack.addEventListener('mouseout', function(){
+  rollBackButton.addEventListener('mouseout', function(){
     this.removeAttribute("style", "opacity: 0.7;")
     
   });
 
-  rollBackButton.addEventListener('click', function(){
-    rollBackButton.setAttribute("style", "color: #FFF;");
+  backButton.addEventListener('click', function(){
+    backButton.setAttribute("style", "color: #FFF;");
   });
 })
